ed17a5f70fca4f4eda12c6975cea4fb5c0617be6
berrang232 <berrang232@unicaen.fr>
2025-07-11 12:15:09 +0200

    end of part 3

--- main.py
+++ main.py
@@ -5,32 +5,19 @@ import matplotlib
 
 # neuron with 4 inputs and a bias
 inputs = [1, 2, 3, 2.5]
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+]
+biases = [2, 3, 0.5]
 
-output = [0, 0, 0]
+# output of one neuron : 
+# weight1 * input1 + weight2 * input2 + ... + bias
 
-# neuron 1
-for i in range(len(inputs)):
-    output[0] += inputs[i] * weights1[i]
+# calculates the output of every neurons of this layer
+layer_outputs = np.dot(weights, inputs) + biases
 
-output[0] += bias1
 
-# neuron 2
-for i in range(len(inputs)):
-    output[1] += inputs[i] * weights2[i]
-
-output[1] += bias2
-
-# neuron 3
-for i in range(len(inputs)):
-    output[2] += inputs[i] * weights3[i]
-
-output[2] += bias3
-
-print(output)
+print(layer_outputs) 
