65eac9bed4f23bd4307a6650873b4be59e140e00
berrang232 <berrang232@unicaen.fr>
2025-07-11 11:12:02 +0200

    end of part 2, code is dirty

--- main.py
+++ main.py
@@ -2,15 +2,35 @@ import sys
 import numpy as np
 import matplotlib
 
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
 
-output = inputs[0] * weights [0] + inputs[1] * weights [1] + inputs[2] * weights [2] + bias
+# neuron with 4 inputs and a bias
+inputs = [1, 2, 3, 2.5]
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+output = [0, 0, 0]
+
+# neuron 1
+for i in range(len(inputs)):
+    output[0] += inputs[i] * weights1[i]
+
+output[0] += bias1
+
+# neuron 2
+for i in range(len(inputs)):
+    output[1] += inputs[i] * weights2[i]
+
+output[1] += bias2
+
+# neuron 3
+for i in range(len(inputs)):
+    output[2] += inputs[i] * weights3[i]
+
+output[2] += bias3
 
 print(output)
-
-'''
-if __name__ == '__main__':
-    pass
-'''
