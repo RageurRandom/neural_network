c94896e4fcd9133a3ea4422d51a41f7142036f23
Leo Berranger <berrang232@unicaen.fr>
2025-07-17 17:17:07 +0200

    end of part 4

--- main.py
+++ main.py
@@ -2,38 +2,29 @@ import sys
 import numpy as np
 import matplotlib
 
-
-# 3 neuron with 4 inputs and a bias
+np.random.seed(0) # TODO remove
 
 # this is a batch of inputs
-inputs = [
-            [1, 2, 3, 2.5],
-            [2.0, 5.0, -1.0, 2.0],
-            [-1.5, 2.7, 3.3, -0.8]
+X = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
 ]
-weights = [
-    [0.2, 0.8, -0.5, 1.0],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87]
-]
-biases = [2, 3, 0.5]
 
-# second layer : 3 neurons with 3 inputs (from the previous layer)
-weights2 = [
-    [0.1, -0.14, 0.5],
-    [-0.5, 0.12, -0.33],
-    [-0.44, 0.73, -0.13]
-]
-biases2 = [-1, 2, -0.5]
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biaises = np.zeros((1, n_neurons)) # in case of a dead network (outputs of 0), this should be changed
 
-# output of one neuron : 
-# weight1 * input1 + weight2 * input2 + ... + bias
-
-# calculates the output of every neurons of this layer
-# We need the transpose of the weights matrix to make the dot product (shape error)
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biaises
 
 
-print(layer1_outputs)
-print(layer2_outputs)
+inputs_nb = len(X[0])
+layer1 = LayerDense(inputs_nb, 5)
+layer2 = LayerDense(5, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+
+print(layer2.output)
