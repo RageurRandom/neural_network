a905052cd6e87970fd1425bbd01800f5b3c3d2ac
Leo Berranger <berrang232@unicaen.fr>
2025-07-21 12:27:24 +0200

    end of part 8

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib
 import nnfs
 from nnfs.datasets import spiral_data
+from abc import ABC, abstractmethod
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -25,6 +26,33 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+
+class Loss(ABC):
+
+    @abstractmethod
+    def forward(self, y_pred, y_true):
+        pass
+
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss  = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7) # avoid issues with 0
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+
+        return negative_log_likelihoods
+
 nnfs.init() # set random seed and default datatype in numpy
 X, y = spiral_data(samples=100, classes=3)
 
@@ -40,3 +68,8 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
 print(activation2.output[:5])
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss:", loss)
